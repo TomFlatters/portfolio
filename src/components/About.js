74aeaede0bf810f0a47bb99e4205d27c73366542
Tom Flatters <tflats.tf@gmail.com>
2019-09-30 17:29:26 +0100

    Mobile views

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import showmore from '../img/expand-filter.svg';
+import showless from '../img/hide-filter.svg';
+
 export default function About(){
+
+    const [expanded, toggleExpanded] = useState(false)
+
     return(
         <div>
+        <div className="full-display">
 
         <h1>About</h1>
         <p>
             Hi, I'm Tom - an undergraduate at Oxford University reading Materials Science. I also like to code - and so I thought I'd put this website together as a place to collate my projects.
+        </p>
+        <p>
+            (You'll also find non-technical stories here.)
         </p>
         <p>
             Most of my experience is in Javascript, Python, and Matlab, although I have done some Lisp, C++, and Java in my time. (I'm always trying to learn more!)
@@ -13,6 +23,46 @@ export default function About(){
         <p>
             Please get in touch if you have any questions!
         </p>
+        <h2>Contact</h2>
+        <ol>
+            <li><a href="/">Email</a></li>
+            <li><a href="/">GitHub</a></li>
+            <li><a href="/">LinkedIn</a></li>
+        </ol>
+        </div>
+
+        <div className="hide">
+            <h1 onClick={() => {
+                let el = document.getElementById("aboutp")
+                if(el.className === "hidep"){
+                    el.classList = "show"
+                } else{
+                    el.classList = "hidep"
+                }
+                toggleExpanded(!expanded)
+            }}>About {expanded ? <img className="toggle-mobile-expansion" src={showmore} alt="Toggle about text"/> : <img className="toggle-mobile-expansion" src={showless} alt="Toggle about text"/>}
+            </h1>
+            <div id="aboutp" className="hidep">
+                <p>
+                    Hi, I'm Tom - an undergraduate at Oxford University reading Materials Science. I also like to code - and so I thought I'd put this website together as a place to collate my projects.
+                </p>
+                <p>
+                    (You'll also find non-technical stories here.)
+                </p>
+                <p>
+                    Most of my experience is in Javascript, Python, and Matlab, although I have done some Lisp, C++, and Java in my time. (I'm always trying to learn more!)
+                </p>
+                <p>
+                    Please get in touch if you have any questions!
+                </p>
+                <h2>Contact</h2>
+                <ol>
+                    <li><a href="/">Email</a></li>
+                    <li><a href="/">GitHub</a></li>
+                    <li><a href="/">LinkedIn</a></li>
+                </ol>
+            </div>
+        </div>
         </div>
 
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ import '../App.css';
 import Post from "./Post.js"
 import { db } from '../Firebase.js'
 import About from './About.js'
+import showmore from '../img/expand-filter.svg';
+import showless from '../img/hide-filter.svg';
 
 function Home(){
 
     const [posts, setPosts] = useState([])
+    const [showingPages, toggleShowingPages] = useState(false)
 
     useEffect(() => {
         getPosts()
@@ -27,14 +30,6 @@ function Home(){
         <div className="home-page-container">
             <div className="home-page-column about-column">
                 <About />
-                <div>
-                    <h2>Contact</h2>
-                    <ol>
-                        <li><a href="/">Email</a></li>
-                        <li><a href="/">GitHub</a></li>
-                        <li><a href="/">LinkedIn</a></li>
-                    </ol>
-                </div>
             </div>
             <div className="home-page-column stories-column">
                 <h1>Stories</h1>
@@ -51,14 +46,44 @@ function Home(){
                 </div>
             </div>
             <div className="home-page-column links-column">
-                <h1>Links</h1>
+                <h1>Pages</h1>
+                <h3>Stories</h3>
                 <ol>
-                    <li><a href="/">Computing In Materials Science</a></li>
-                    <li><a href="/">Getting Started With React</a></li>
-                    <li><a href="/">Investing</a></li>
-                    <li><a href="/">teachyourselfcs</a></li>
-                    <li><a href="/">FreeCodeCamp</a></li>
+                    <li>How I made Google for counsellors</li>
+                    <li>A journey from speech to text</li>
+                    <li>How we're opening up Oxbridge for all</li>
+                    <li>OX1: a startup incubator run by students</li>
+                    <li></li>
                 </ol>
+                <h3>Helpful Resources</h3>
+                <ol>
+                    <li><a href="https://www.teachyourselfcs.com">teachyourselfcs</a></li>
+                    <li><a href="https://www.freecodecamp.org">FreeCodeCamp</a></li>
+                </ol>
+            </div>
+            <div className="home-page-column hide">
+            <h1 onClick={() => {
+                toggleShowingPages(!showingPages)
+            }}>Pages {showingPages ? <img className="toggle-mobile-expansion" src={showmore} alt="Toggle showing pages section"/> : <img className="toggle-mobile-expansion" src={showless} alt="Toggle showing pages section"/>}</h1>
+                { showingPages ?
+                (
+                <div>
+                    <h3 className="mobile-links-header">Stories</h3>
+                    <ol>
+                        <li>How I made Google for counsellors</li>
+                        <li>A journey from speech to text</li>
+                        <li>How we're opening up Oxbridge for all</li>
+                        <li>OX1: a startup incubator run by students</li>
+                        <li></li>
+                    </ol>
+                    <h3 className="mobile-links-header">Helpful Resources</h3>
+                    <ol>
+                        <li><a href="https://www.teachyourselfcs.com">teachyourselfcs</a></li>
+                        <li><a href="https://www.freecodecamp.org">FreeCodeCamp</a></li>
+                    </ol>
+                </div>
+                ) : ("")}
+                
             </div>
         </div>
     )
